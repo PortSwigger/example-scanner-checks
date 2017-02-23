2f79bd8e02feeee4b264c093c34fcf505d3e7c90
Adam Piper <adam.piper@portswigger.net>
2017-02-23 16:23:32 +0900

    Adhere more to familar language idioms.

--- ruby/CustomScannerChecks.rb
+++ ruby/CustomScannerChecks.rb
@@ -24,13 +24,13 @@ class BurpExtender
     @callbacks = callbacks
 
     # obtain an extension helpers object
-    @helpers = callbacks.getHelpers()
+    @helpers = callbacks.getHelpers
 
     # set our extension name
-    callbacks.setExtensionName("Custom scanner checks")
+    callbacks.setExtensionName "Custom scanner checks"
 
     # register ourselves as a custom scanner check
-    callbacks.registerScannerCheck(self)
+    callbacks.registerScannerCheck self
   end
 
   # helper method to search a response for occurrences of a literal match string
@@ -42,7 +42,7 @@ class BurpExtender
     while start < response.length
       start = @helpers.indexOf(response, match, true, start, response.length)
       break if start == -1
-      matches.add([start, start + match.length].to_java :int)
+      matches.add [start, start + match.length].to_java :int
       start += match.length
     end
 
@@ -55,30 +55,30 @@ class BurpExtender
 
   def doPassiveScan(baseRequestResponse)
     # look for matches of our passive check grep string
-    matches = self._get_matches(baseRequestResponse.getResponse(), GREP_STRING_BYTES)
+    matches = self._get_matches(baseRequestResponse.getResponse, GREP_STRING_BYTES)
     return nil if matches.length == 0
 
     # report the issue
     issues = ArrayList.new
     issues.add CustomScanIssue.new(
-      baseRequestResponse.getHttpService(),
-      @helpers.analyzeRequest(baseRequestResponse).getUrl(),
+      baseRequestResponse.getHttpService,
+      @helpers.analyzeRequest(baseRequestResponse).getUrl,
       [@callbacks.applyMarkers(baseRequestResponse, nil, matches)],
       "CMS Info Leakage",
       "The response contains the string: " + GREP_STRING,
-      "Information").to_java(IScanIssue)
+      "Information").to_java IScanIssue
 
     return issues
   end
 
   def doActiveScan(baseRequestResponse, insertionPoint)
     # make a request containing our injection test in the insertion point
-    checkRequest = insertionPoint.buildRequest(INJ_TEST)
+    checkRequest = insertionPoint.buildRequest INJ_TEST
     checkRequestResponse = @callbacks.makeHttpRequest(
-        baseRequestResponse.getHttpService(), checkRequest)
+        baseRequestResponse.getHttpService, checkRequest)
 
     # look for matches of our active check grep string
-    matches = self._get_matches(checkRequestResponse.getResponse(), INJ_ERROR_BYTES)
+    matches = self._get_matches(checkRequestResponse.getResponse, INJ_ERROR_BYTES)
     return nil if matches.length == 0
 
     # get the offsets of the payload within the request, for in-UI highlighting
@@ -87,12 +87,12 @@ class BurpExtender
     # report the issue
     issues = ArrayList.new
     issues.add CustomScanIssue.new(
-      baseRequestResponse.getHttpService(),
-      @helpers.analyzeRequest(baseRequestResponse).getUrl(),
+      baseRequestResponse.getHttpService,
+      @helpers.analyzeRequest(baseRequestResponse).getUrl,
       [@callbacks.applyMarkers(checkRequestResponse, requestHighlights, matches)],
       "Pipe injection",
       "Submitting a pipe character returned the string: " + INJ_ERROR,
-      "High").to_java(IScanIssue)
+      "High").to_java IScanIssue
 
     return issues
   end
@@ -106,7 +106,7 @@ class BurpExtender
     # Since the issue name is sufficient to identify our issues as different,
     # if both issues have the same name, only report the existing issue
     # otherwise report both issues
-    if existingIssue.getIssueName() == newIssue.getIssueName()
+    if existingIssue.getIssueName == newIssue.getIssueName
       return -1
     else
       return 0
